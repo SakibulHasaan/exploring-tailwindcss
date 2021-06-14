5712bd70c77033a3c22be28c533df753cd6665a5
Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
2021-06-14 22:45:13 +0600

    Tailwind integrated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Tailwind</h1>
+      <h1 className="text-4xl">Hello Tailwind</h1>
+      <p className=''>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, deserunt. Animi beatae accusantium, repellat blanditiis soluta quod unde doloremque autem.</p>
     </div>
   );
 }
